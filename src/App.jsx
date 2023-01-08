53670e3d5d24202133c4b4cdcbfc37d3eedc4a06
“karsujo” <karsujo@gmail.com>
2023-01-08 15:55:39 -0500

    Fix: base url quotes

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <ThemeProvider theme={darkMode.value ? darkTheme : lightTheme}>
         <GlobalStyles />
         <div className="App">
-          <BrowserRouter basename='/portfolio-site'>
+          <BrowserRouter basename="/portfolio-site">
             <MainApp />
           </BrowserRouter>
         </div>
